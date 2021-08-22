ce549bd99663b43f5ed0039768f5b6b72e98786f
saltyposaune <ewalkertbone@gmail.com>
2021-08-21 22:26:48 -0600

    fixed notes.html bug

--- routes/routes.js
+++ routes/routes.js
@@ -3,23 +3,39 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../develop/public/index.html'));
+    res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
 });
 
 // GET /notes should return the notes.html file.
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
 });
 
 // GET * should return the index.html file.
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../develop/public/index.html'));
+    res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
 });
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
+router.get('/api/notes', function(req, res) {
+    res.json(notes);
+});
 
-// POST /api/notes shoudsld receive a new note to save on the request body, 
-// add it to the db.json file, and then return the new note to the client. 
+router.get('/api/notes/:id', (req, res) => {
+    res.json((notes[req.params.id]));
+});
+
+// POST /api/notes should receive a new note to save on the request body, 
+// add it to the db.json file, and then return the new note to the client.
+router.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    notes.push(newNote);
+    createNewNote()
+});
+
+function createNewNote() {
+    fs.writeFile('/Develop/db/db.json', JSON.stringify(notes))
+}
 // You'll need to find a way to give each note a unique id when it's saved 
 // (look into npm packages that could do this for you).
 
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const path = require('path');
-const { notes } = require('./db/db')
+const { notes } = require('./Develop/db/db')
 const express = require('express');
 const PORT = process.env.PORT || 3003
 const app = express();
 const routes = require('./routes/routes');
 
 app.use(express.static(__dirname));
+app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', routes);
