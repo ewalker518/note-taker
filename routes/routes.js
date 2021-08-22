82bf40fb92ed70fa03ff3ed8d502229475675436
saltyposaune <ewalkertbone@gmail.com>
2021-08-22 13:35:55 -0600

    fixed createNewNote function

--- routes/routes.js
+++ routes/routes.js
@@ -2,40 +2,49 @@ const fs = require('fs')
 const path = require('path');
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+fs.readFile('db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data)
 
-// GET /notes should return the notes.html file.
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
+    router.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
 
-// GET * should return the index.html file.
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+    // GET /notes should return the notes.html file.
+    router.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
 
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
-router.get('/api/notes', function(req, res) {
-    res.json(notes);
-});
+    // GET * should return the index.html file.
+    router.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
 
-router.get('/api/notes/:id', (req, res) => {
-    res.json((notes[req.params.id]));
-});
+    // GET /api/notes should read the db.json file and return all saved notes as JSON.
+    router.get('/api/notes', function (req, res) {
+        res.json(notes);
+    });
 
-// POST /api/notes should receive a new note to save on the request body, 
-// add it to the db.json file, and then return the new note to the client.
-router.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    notes.push(newNote);
-    createNewNote()
-});
+    router.get('/api/notes/:id', (req, res) => {
+        res.json((notes[req.params.id]));
+    });
 
-function createNewNote() {
-    fs.writeFile('./db/db.json', JSON.stringify(notes))
-}
+    // POST /api/notes should receive a new note to save on the request body, 
+    // add it to the db.json file, and then return the new note to the client.
+    router.post('/api/notes', (req, res) => {
+        const newNote = req.body;
+        notes.push(newNote);
+        createNewNote();
+        // res.json(note);
+    });
+
+    function createNewNote() {
+        fs.writeFile('db/db.json', JSON.stringify(notes, '\t'), err => {
+            if (err) throw err;
+            return true;
+        });
+    };
+});
 // You'll need to find a way to give each note a unique id when it's saved 
 // (look into npm packages that could do this for you).
 
