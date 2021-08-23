6dd482fb8f9a0cf805ca803d0662244b92b2f85f
saltyposaune <ewalkertbone@gmail.com>
2021-08-22 20:50:45 -0600

    deploy to heroku

--- routes/routes.js
+++ routes/routes.js
@@ -1,51 +1,66 @@
 const fs = require('fs')
 const path = require('path');
 const router = require('express').Router();
+const notes = require('../db/db');
+const generateUniqueId = require('generate-unique-id');
 
-fs.readFile('db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    var notes = JSON.parse(data)
-
-    router.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-
-    // GET /notes should return the notes.html file.
-    router.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
-    });
-
-    // GET * should return the index.html file.
-    router.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-
-    // GET /api/notes should read the db.json file and return all saved notes as JSON.
-    router.get('/api/notes', function (req, res) {
-        res.json(notes);
-    });
-
-    router.get('/api/notes/:id', (req, res) => {
-        res.json((notes[req.params.id]));
-    });
-
-    // POST /api/notes should receive a new note to save on the request body, 
-    // add it to the db.json file, and then return the new note to the client.
-    router.post('/api/notes', (req, res) => {
-        const newNote = req.body;
-        notes.push(newNote);
-        createNewNote();
-        // res.json(note);
-    });
-
-    function createNewNote() {
-        fs.writeFile('db/db.json', JSON.stringify(notes, '\t'), err => {
-            if (err) throw err;
-            return true;
-        });
-    };
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+router.get('/api/notes', function (req, res) {
+    res.json(notes);
 });
-// You'll need to find a way to give each note a unique id when it's saved 
-// (look into npm packages that could do this for you).
+
+router.get('/api/notes/:id', (req, res) => {
+    res.json((notes[req.params.id]));
+});
+
+// POST /api/notes should receive a new note to save on the request body, 
+// add it to the db.json file, and then return the new note to the client.
+router.post('/api/notes', (req, res) => {
+    console.log(notes);
+    const newNote = req.body;
+    const id = generateUniqueId({ // https://www.npmjs.com/package/generate-unique-id
+        length: 5
+       });
+    newNote.id = id;
+    notes.push(newNote);
+    updateNotes();
+    res.json(newNote);
+});
+
+function updateNotes() {
+    fs.writeFile('db/db.json', JSON.stringify(notes, '\t'), err => {
+        if (err) throw err;
+        return true;
+    });
+};
+
+// function copied from https://stackoverflow.com/questions/3396088/how-do-i-remove-an-object-from-an-array-with-javascript
+var removeByAttr = function(arr, attr, value){
+    var i = arr.length;
+    while(i--){
+       if( arr[i] 
+           && arr[i].hasOwnProperty(attr) 
+           && (arguments.length > 2 && arr[i][attr] === value ) ){ 
+
+           arr.splice(i,1);
+           return arr;
+       }
+    }
+}
+
+router.delete('/api/notes/:id', (req, res) => {
+    removeByAttr(notes, 'id', req.params.id);
+    updateNotes();
+    res.json(notes);
+})
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// GET /notes should return the notes.html file.
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { notes } = require('./db/db')
+const notes = require('./db/db')
 const express = require('express');
 const PORT = process.env.PORT || 3003
 const app = express();
@@ -8,11 +8,11 @@ const routes = require('./routes/routes');
 
 app.use(express.static(__dirname));
 app.use(express.static('public'));
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', routes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port http://localhost:${PORT}`);
-})
+});
